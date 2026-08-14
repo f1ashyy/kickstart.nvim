cea59b6ff48efe25cf7d006b0490ee63bac85459
flashy <71889536+f1ashyy@users.noreply.github.com>
2026-08-15 00:53:04 +0500

    better statusline

--- lua/custom/plugins/blink-cmp.lua
+++ lua/custom/plugins/blink-cmp.lua
@@ -2,14 +2,7 @@
 -- vim.pack.add { { src = gh 'saghen/blink.cmp', version = vim.version.range '1.*' } }
 require('blink.cmp').setup {
   keymap = {
-    -- 'default' (recommended) for mappings similar to built-in completions
-    --   <c-y> to accept ([y]es) the completion.
-    --    This will auto-import if your LSP supports it.
-    --    This will expand snippets if the LSP sent a snippet.
-    -- 'super-tab' for tab to accept
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
+
     -- For an understanding of why the 'default' preset is recommended,
     -- you will need to read `:help ins-completion`
     --
@@ -35,10 +28,20 @@ require('blink.cmp').setup {
     nerd_font_variant = 'mono',
   },
 
+  cmdline = {
+    keymap = {
+      preset = 'cmdline',
+    },
+    completion = {
+      menu = {
+        auto_show = true,
+      },
+    },
+  },
   completion = {
     -- By default, you may press `<c-space>` to show the documentation.
     -- Optionally, set `auto_show = true` to show the documentation after a delay.
-    documentation = { auto_show = false, auto_show_delay_ms = 500 },
+    documentation = { auto_show = true, auto_show_delay_ms = 500 },
   },
 
   sources = {
